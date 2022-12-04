1dca43fb4cf7d973c1cd31397b056883250d39d6
Yash <68184600+1yashsharma1@users.noreply.github.com>
2022-12-04 20:31:43 +0530

    Time: 3 ms (87.53%), Space: 7 MB (52.93%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,50 +1,32 @@
 class Solution {
 public:
-int myAtoi(string s) {
-   int sign=1;
-   int flag=0;
-   int i=0;
-   int res=0;
-   
-  while(flag==0)
-  {
-    if(s[i]=='-'||s[i]=='+'||isdigit(s[i]))
-    {
-     s=s.substr(i,s.length());
-      break;
-    }
-    if(s[i]==' ')
-    {
-     
-    }
-    else{
-      return 0;
-    }
-    i++;
-  }
- 
-  i=(s[0]=='+'||s[0]=='-')?1:0;
-  sign=(s[0]=='-')?-1:1;
-  string newString=s;
-  
-  for(i;i<newString.length();i++)
-  {
-    if(s[i]==' '||!isdigit(s[i]))
-    {
-      break;
-    }
-    int temp=(newString[i]-'0')*sign;
-    if((sign==1)&&((res==INT_MAX/10&&temp>=7)||(res>INT_MAX/10)))
-    {
-      return INT_MAX;
-    }
-    if((sign==-1)&&((res==INT_MIN/10&&temp<=-8)||(res<INT_MIN/10)))
-    {
-      cout<<res;
-      return INT_MIN;
-    }
-    res=res*10+temp;
-  }
-  return res;
+ int myAtoi(string s) {
+        if(s.length()==0) return 0;
+
+        int i=0;
+         while(i<s.size() && s[i]== ' ') {
+             i++;
+         }     
+        s = s.substr(i); //i ---> last of string
+        
+        int sign = +1;
+        long ans = 0;
+        
+        if(s[0] == '-') sign = -1;
+        
+        int MAX = INT_MAX, MIN = INT_MIN;
+        i = (s[0] == '+' || s[0] == '-') ? 1 : 0;
+		
+        while(i < s.length()) {
+            if(s[0] == ' ' || !isdigit(s[i])) break;
+            
+            ans = ans * 10 + s[i]-'0';
+            if(sign == -1 && -1*ans < MIN) return MIN;
+            if(sign == 1 && ans > MAX) return MAX;
+            
+            i++;
+        }
+        
+        return (int)(sign*ans);
     }
 };
