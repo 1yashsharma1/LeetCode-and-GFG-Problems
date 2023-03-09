a415b18af172615a74a045ac82f45b625236af2c
Yash <68184600+1yashsharma1@users.noreply.github.com>
2023-03-09 20:43:23 +0530

    Time: 3 ms (73.78%), Space: 7.5 MB (69.27%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+   void findsubsetsWithDup(int index,  vector<int> &nums, vector<int> &ds,vector<vector<int>> &res)
+{
+  res.push_back(ds);
+  for(int i=index;i<nums.size();i++){
+    if(i!=index && nums[i]==nums[i-1]){
+      continue;
+    }
+    ds.push_back(nums[i]);
+    findsubsetsWithDup(i+1,nums,ds,res);
+    ds.pop_back();
+
+  }
+ 
+}
+
+vector<vector<int>> subsetsWithDup(vector<int> &nums)
+{
+  vector<int> ds;
+  vector<vector<int>> res;
+  sort(nums.begin(),nums.end());
+  findsubsetsWithDup(0,nums,ds,res);
+  return res;
+}
+};
